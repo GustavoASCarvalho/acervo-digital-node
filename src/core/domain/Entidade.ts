ae0f232e6317760c9b55eba93dafff320562d744
Gustavo Alexandre <gustavoalexandrescarvalho@gmail.com>
2022-08-28 12:28:08 -0300

    feat: adicionado entidade padrao do projeto

--- src/core/domain/Entidade.ts
+++ src/core/domain/Entidade.ts
@@ -1,27 +1,38 @@
-import crypto from "crypto";
+import crypto from 'crypto';
 
 export abstract class Entidade<T> {
-  protected _id: string;
-  protected _criadoEm: Date;
-  protected _atualizadoEm: Date;
-  protected _deletadoEm?: Date;
-  public props: T;
+	protected _id: string;
+	protected _criadoEm: Date;
+	protected _atualizadoEm: Date;
+	protected _deletadoEm: Date | undefined;
+	public props: T;
 
-  get id() {
-    return this._id;
-  }
+	get id() {
+		return this._id;
+	}
 
-  constructor(
-    props: T,
-    id?: string,
-    criadoEm?: Date,
-    atualizadoEm?: Date,
-    deletadoEm?: Date
-  ) {
-    this.props = props;
-    this._id = id ?? crypto.randomUUID();
-    this._criadoEm = criadoEm ?? new Date();
-    this._atualizadoEm = atualizadoEm ?? new Date();
-    this._deletadoEm = deletadoEm ?? undefined;
-  }
+	get criadoEm() {
+		return this._criadoEm;
+	}
+
+	get atualizadoEm() {
+		return this._atualizadoEm;
+	}
+	get deletadoEm() {
+		return this._deletadoEm;
+	}
+
+	constructor(
+		props: T,
+		id?: string,
+		criadoEm?: Date,
+		atualizadoEm?: Date,
+		deletadoEm?: Date,
+	) {
+		this.props = props;
+		this._id = id ?? crypto.randomUUID();
+		this._criadoEm = criadoEm ?? new Date();
+		this._atualizadoEm = atualizadoEm ?? new Date();
+		this._deletadoEm = deletadoEm ?? undefined;
+	}
 }
