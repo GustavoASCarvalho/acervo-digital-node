bcdf5521fb2937b1addfce4be795bf7023c9804e
Gustavo Alexandre <gustavoalexandrescarvalho@gmail.com>
2022-08-28 16:07:16 -0300

    chore(server): nome de variavel

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import cors from 'cors';
 import express from 'express';
-import { router } from './routes/index.routes';
+import { rotas } from './routes/index.routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use(rotas);
 
 app.listen(process.env.PORT || 3333, () => {
 	console.log('Server started on port http://localhost:3333 !');
