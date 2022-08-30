7fe69967a2cbc3629522226f91d5ce9fce048000
Gustavo Alexandre <gustavoalexandrescarvalho@gmail.com>
2022-08-30 11:28:16 -0300

    feat: mudando banco de dados de sqlite para pg

--- prisma/migrations/20220830024627_init/migration.sql
+++ prisma/migrations/20220830024627_init/migration.sql
@@ -1,19 +1,21 @@
 -- CreateTable
 CREATE TABLE "Usuario" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "senha" TEXT NOT NULL,
     "imagemDePerfil" TEXT NOT NULL,
     "cargo" TEXT NOT NULL,
-    "criadoEm" DATETIME NOT NULL,
-    "atualizadoEm" DATETIME NOT NULL,
-    "deletadoEm" DATETIME
+    "criadoEm" TIMESTAMP(3) NOT NULL,
+    "atualizadoEm" TIMESTAMP(3) NOT NULL,
+    "deletadoEm" TIMESTAMP(3),
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Imagem" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
     "data" TEXT NOT NULL,
@@ -22,32 +24,38 @@ CREATE TABLE "Imagem" (
     "latitude" TEXT NOT NULL,
     "longitude" TEXT NOT NULL,
     "idDoUsuario" TEXT NOT NULL,
-    "criadoEm" DATETIME NOT NULL,
-    "atualizadoEm" DATETIME NOT NULL,
-    "deletadoEm" DATETIME
+    "criadoEm" TIMESTAMP(3) NOT NULL,
+    "atualizadoEm" TIMESTAMP(3) NOT NULL,
+    "deletadoEm" TIMESTAMP(3),
+
+    CONSTRAINT "Imagem_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Postagem" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "titulo" TEXT NOT NULL,
     "descricao" TEXT NOT NULL,
     "texto" TEXT NOT NULL,
     "visualizacoes" INTEGER NOT NULL,
     "idDoUsuario" TEXT NOT NULL,
-    "criadoEm" DATETIME NOT NULL,
-    "atualizadoEm" DATETIME NOT NULL,
-    "deletadoEm" DATETIME
+    "criadoEm" TIMESTAMP(3) NOT NULL,
+    "atualizadoEm" TIMESTAMP(3) NOT NULL,
+    "deletadoEm" TIMESTAMP(3),
+
+    CONSTRAINT "Postagem_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Tag" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
     "idDoUsuario" TEXT NOT NULL,
-    "criadoEm" DATETIME NOT NULL,
-    "atualizadoEm" DATETIME NOT NULL,
-    "deletadoEm" DATETIME
+    "criadoEm" TIMESTAMP(3) NOT NULL,
+    "atualizadoEm" TIMESTAMP(3) NOT NULL,
+    "deletadoEm" TIMESTAMP(3),
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
